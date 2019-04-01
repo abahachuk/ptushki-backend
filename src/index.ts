3b60d54b53f6579d75c6a8e70e62d39c208129b5
Mikhail Hozhy <mikhail_hozhy@epam.com>
2019-04-01 22:20:26 +0300

    EPMCUXDA-6483: extract app for future testability

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,30 @@
 // @ts-ignore
 import 'dotenv/config';
 import config from 'config';
+import { Application } from 'express';
+
 import app from './app';
 import connectDB from './db';
 import routes from './routes';
 
 const PORT = config.get('PORT');
 
-(async () => {
-  try {
-    await connectDB();
-    app.use(routes());
-    app.listen(PORT, () => {
-      console.log(`App is listened at ${PORT}`);
-    });
-  } catch (e) {
-    console.error(e);
-  }
-})();
+const createApp = async (): Promise<Application> => {
+  await connectDB();
+  app.use(routes());
+  return app;
+};
+
+if (!module.parent) {
+  (async () => {
+    try {
+      (await createApp()).listen(PORT, () => {
+        console.log(`App is listened at ${PORT}`);
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  })();
+}
+
+export default createApp;
