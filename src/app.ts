d070ed1c6980f94984e7566812dbe8407cc60958
Mikhail Hozhy <mikhail_hozhy@epam.com>
2019-04-02 01:29:42 +0300

    EPMCUXDA-6483: update responsibilty between app & index
    improve testablity & invert db dependency

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
-import express from 'express';
+import express, { Application } from 'express';
 import bodyParser from 'body-parser';
+import routes from './routes';
 
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const createApp = async (connectDb: () => Promise<any>): Promise<Application> => {
+  await connectDb();
+  const app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(routes());
+  return app;
+};
 
-export default app;
+export default createApp;
--- src/index.ts
+++ src/index.ts
@@ -3,28 +3,15 @@ import 'dotenv/config';
 import config from 'config';
 import { Application } from 'express';
 
-import app from './app';
+import createApp from './app';
 import connectDB from './db';
-import routes from './routes';
 
 const PORT = config.get('PORT');
 
-const createApp = async (): Promise<Application> => {
-  await connectDB();
-  app.use(routes());
-  return app;
-};
-
-if (!module.parent) {
-  (async () => {
-    try {
-      (await createApp()).listen(PORT, () => {
-        console.log(`App is listened at ${PORT}`);
-      });
-    } catch (e) {
-      console.error(e);
-    }
-  })();
-}
-
-export default createApp;
+createApp(connectDB)
+  .then((app: Application) => {
+    app.listen(PORT, () => {
+      console.log(`App is listened at ${PORT}`);
+    });
+  })
+  .catch(console.error);
