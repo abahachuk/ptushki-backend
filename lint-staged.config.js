dafa00c25704a8b2494e3eb137ef6ca86a08dfca
Mikhail Hozhy <mikhail_hozhy@epam.com>
2019-04-02 02:23:02 +0300

    EPMCUXDA-6483: add js files to lint-staged support

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.ts': ['eslint --fix', 'git add'],
+  '*.{ts,js}': ['eslint --fix', 'git add'],
   '*.{json,md}': ['prettier --write', 'git add'],
   '{.eslintrc}': ['prettier --write', 'git add'],
 };
