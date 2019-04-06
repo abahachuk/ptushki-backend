aad42b0dda2aaf15e879f1dda7866850b5a23971
Mikhail Hozhy <mikhail_hozhy@epam.com>
2019-04-06 04:51:59 +0300

    EPMCUXDA-6598: update user and add processor for user fixture

---
+++ src/fixtures/UserProcessor.ts
@@ -0,0 +1,17 @@
+import { IProcessor } from 'typeorm-fixtures-cli';
+import { User, UserRole } from '../entities/user-entity';
+
+/* eslint-disable class-methods-use-this */
+export default class UserProcessor implements IProcessor<User> {
+  public preProcess(_name: string, object: any): any {
+    console.log(_name);
+    if (!object.role) {
+      return { ...object, role: Math.random() > 0.5 ? UserRole.Observer : UserRole.Ringer };
+    }
+    return object;
+  }
+
+  public async postProcess(_name: string, user: { password: string } & User): Promise<void> {
+    await user.setPassword(user.password as string);
+  }
+}
