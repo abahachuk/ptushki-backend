5b30a8071ef05eeda8994877e042797cbba7f95c
Mikhail Hozhy <mikhail_hozhy@epam.com>
2019-04-09 23:09:59 +0300

    update runner
    - add async helper
    - extract connection from loader
    - improve error handling

--- src/db/run-fixtures.ts
+++ src/db/run-fixtures.ts
@@ -1,26 +1,23 @@
 import path from 'path';
 import { Builder, fixturesIterator, IFixture, Loader, Parser, Resolver } from 'typeorm-fixtures-cli/dist';
-import { createConnection, getRepository } from 'typeorm';
+import { Connection, createConnection } from 'typeorm';
 import config from './prepare-db-config';
+import { executeInThreadedQueue } from '../utils/async-queue';
 
-const load = async (): Promise<{ [index: string]: number }> => {
+const load = async (connection: Connection): Promise<{ [index: string]: number }> => {
   const loader = new Loader();
   loader.load(path.resolve(process.cwd(), 'src/fixtures'));
 
   const resolver = new Resolver();
-  const connection = await createConnection(config);
-
-  // README this drops db and recreate scheme
-  await connection.synchronize(true);
   const fixtures = resolver.resolve(loader.fixtureConfigs);
   const builder = new Builder(connection, new Parser());
 
-  const fixturing = [...fixturesIterator(fixtures)].map(async (f: IFixture) => {
+  const fixturing = [...fixturesIterator(fixtures)].map((f: IFixture) => async () => {
     const entity = await builder.build(f);
-    await getRepository(entity.constructor.name).save(entity);
+    await connection.getRepository(entity.constructor.name).save(entity);
   });
 
-  await Promise.all(fixturing);
+  await executeInThreadedQueue(fixturing, 5);
 
   const result = fixtures.reduce((acc: any, { entity }: { entity: string }) => {
     acc[entity] = acc[entity] ? (acc[entity] += 1) : 1;
@@ -31,8 +28,22 @@ const load = async (): Promise<{ [index: string]: number }> => {
   return result;
 };
 
-load()
-  .then(result => {
-    console.log('fixtures was loaded: ', result);
-  })
-  .catch(console.error);
+let connection: Connection | undefined;
+
+(async () => {
+  try {
+    console.time('taken time');
+    connection = await createConnection(config);
+    // README this drops db and recreate scheme
+    await connection.synchronize(true);
+    const result = await load(connection);
+    console.log('loaded fixtures: ', result);
+    console.timeEnd('taken time');
+  } catch (e) {
+    if (connection) {
+      await connection.close();
+    }
+    console.error(e);
+    process.exit(1);
+  }
+})();
