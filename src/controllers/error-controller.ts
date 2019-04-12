9bfb015264bf724d99c82173d3d98e30940b787a
Dzmitry_Slabko <dzmitry.slabko@tele2.com>
2019-04-12 11:13:44 +0300

    - fixes error-controller logging

--- src/controllers/error-controller.ts
+++ src/controllers/error-controller.ts
@@ -3,12 +3,9 @@ import { logger } from '../configs/logger';
 
 export default (err: any, _req: Request, res: Response, next: NextFunction): void => {
   if (err) {
-    try {
-      logger.error(JSON.stringify(err));
-    } catch {
-      logger.error(err.message);
-    }
+    logger.error(err.message);
     res.status(err.status || 500).json({ error: err.message });
   }
+
   next();
 };
