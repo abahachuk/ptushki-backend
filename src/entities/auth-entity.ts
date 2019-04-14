6da75fc5da8381a2721dca46a86864537e1df312
SerafimovichEugene <Eugene.serafimovich@gmail.com>
2019-04-14 07:21:22 +0300

    EPMCUXDA-6490: update auth entity

--- src/entities/auth-entity.ts
+++ src/entities/auth-entity.ts
@@ -3,8 +3,9 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 @Entity()
 export class RefreshToken {
   // eslint-disable-next-line no-useless-constructor,no-empty-function
-  public constructor(token: string) {
+  public constructor(token: string, userId: string) {
     this.token = token;
+    this.userId = userId;
   }
 
   @PrimaryGeneratedColumn('uuid')
@@ -12,4 +13,7 @@ export class RefreshToken {
 
   @Column({ type: 'varchar' })
   public token: string;
+
+  @Column({ type: 'varchar' })
+  public userId: string;
 }
