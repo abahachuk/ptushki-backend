d10c79faabf1e5c24ba13958d2c2bff05206e984
Dzianis_Dashkevich <fanfasiuss@gmail.com>
2019-04-20 23:02:59 +0300

    EPMCUXDA-6597: add key words list

---
+++ tools/swagger/parsers/keyWords.js
@@ -0,0 +1,11 @@
+module.exports = {
+  CLASS: 'class',
+  IMPLEMENTS: 'implements',
+  PUBLIC: 'public',
+  ARRAY: '[]',
+  OPEN_BRACKET: '(',
+  START_COMMENT: '/**',
+  END_COMMENT: '*/',
+  OPEN_CURLY_BRACKET: '{',
+  CLOSE_CURLY_BRACKET: '}',
+};
