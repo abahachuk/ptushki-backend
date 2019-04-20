2d0e2cd81daae0556f7a76ae9733b54fc26e97e3
Dzianis_Dashkevich <fanfasiuss@gmail.com>
2019-04-20 23:02:19 +0300

    EPMCUXDA-6597: add swagger defaults

---
+++ tools/swagger/defaults.js
@@ -0,0 +1,10 @@
+module.exports = {
+  swagger: '2.0',
+  info: {
+    description: 'This is a sample for ptushki-backend API documentation',
+    version: '1.0.0',
+    title: 'Ptushki API',
+  },
+  host: 'localhost',
+  schemes: ['http'],
+};
