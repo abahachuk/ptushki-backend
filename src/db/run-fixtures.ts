006eab93e5ab516245a7df467578dea3ae188e78
Mikhail Hozhy <mikhail_hozhy@epam.com>
2019-04-27 03:01:09 +0300

    EPMCUXDA-6483 add dot progress & rename exports

--- src/db/run-fixtures.ts
+++ src/db/run-fixtures.ts
@@ -4,7 +4,7 @@ import { Builder, fixturesIterator, IFixture, Loader, Parser, Resolver } from 't
 import { Connection, createConnection } from 'typeorm';
 import config from './prepare-db-config';
 import { executeInThreadedQueue } from '../utils/async-queue';
-import cleanAndWriteLine from '../utils/clean-write-line';
+import { countedProgress, dotProgress } from '../utils/clean-write-line';
 
 const load = async (connection: Connection): Promise<{ [index: string]: number }> => {
   const loader = new Loader();
@@ -17,7 +17,7 @@ const load = async (connection: Connection): Promise<{ [index: string]: number }
   const fixturing = [...fixturesIterator(fixtures)].map((f: IFixture, i: number, arr: IFixture[]) => async () => {
     const entity = await builder.build(f);
     await connection.getRepository(entity.constructor.name).save(entity);
-    cleanAndWriteLine(i, arr.length, 'fixtures loaded:');
+    countedProgress(i, arr.length, 'fixtures loaded:');
   });
 
   await executeInThreadedQueue(fixturing, 5);
@@ -35,13 +35,19 @@ let connection: Connection | undefined;
 
 (async () => {
   try {
+    const progress1 = dotProgress('connect to db');
     console.time('taken time');
     connection = await createConnection(config);
+    progress1.stop();
+    console.log('db connected');
+    const progress2 = dotProgress('drop and sync db');
     // README this drops db and recreate scheme
     await connection.synchronize(true);
+    progress2.stop();
+    console.log('db dropped and synced');
     const result = await load(connection);
+    console.log('loaded fixtures:\n', result, '\n');
     console.timeEnd('taken time');
-    console.log('loaded fixtures: ', result);
   } catch (e) {
     if (connection) {
       await connection.close();
--- src/utils/clean-write-line.ts
+++ src/utils/clean-write-line.ts
@@ -1,6 +1,6 @@
 export const cleanLine = () => process.stdout.write('\r\x1b[K');
 
-export default (current: number, all: number, message: string) => {
+export const countedProgress = (current: number, all: number, message: string) => {
   if (current !== 0) {
     cleanLine();
   }
@@ -8,4 +8,21 @@ export default (current: number, all: number, message: string) => {
   if (current + 1 === all) {
     cleanLine();
   }
+};
+
+export const dotProgress = (message: string) => {
+  let current = 0;
+  const timer = setInterval(() => {
+    if (current !== 0) {
+      cleanLine();
+    }
+    current += 1;
+    process.stdout.write(`${message} ${'.'.repeat(current % 5)}`);
+  }, 300);
+  return {
+    stop: () => {
+      clearInterval(timer);
+      cleanLine();
+    },
+  };
 };
