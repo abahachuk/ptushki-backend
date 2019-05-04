81c54d5754dcda2885a80de015cd7a8adbfcd826
Mikhail Hozhy <mikhail_hozhy@epam.com>
2019-05-04 05:17:51 +0300

    EPMCUXDA-6609 add: suite for multidevice accessing

--- src/controllers/auth-controller.spec.ts
+++ src/controllers/auth-controller.spec.ts
@@ -7,6 +7,8 @@ import { signTokens } from '../services/auth-service';
 import { UserRole, User } from '../entities/user-entity';
 import { RefreshToken } from '../entities/auth-entity';
 
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
 let connection: Connection | undefined;
 let app: Application;
 
@@ -15,6 +17,7 @@ const urls: { [index: string]: string } = {
   signup: '/auth/signup',
   refresh: '/auth/refresh',
   logout: '/auth/logout',
+  test: '/auth/test',
 };
 
 jest.setTimeout(30000);
@@ -241,6 +244,137 @@ describe('Auth', () => {
         .send({ refreshToken });
 
       expect(res.status).toEqual(401);
+    });
+  });
+
+  describe('on multidevices', () => {
+    let user: User;
+    let userId: string;
+    let userRole: UserRole;
+    const email = 'multi-test@mail.com';
+    const password = '12345';
+
+    beforeAll(async () => {
+      user = await User.create({ email, password });
+      user = await userRepository.save(user);
+      ({ id: userId, role: userRole } = user);
+    });
+
+    it('should be able to login. And then refresh his access independently', async () => {
+      const logins = await Promise.all(
+        [0, 1100].map(n =>
+          // README jsonwebtoken cant handle ms precision for timestamps
+          // that can cause refresh tokens collisions if somebody simultaneously login
+          // FIXME can be resolved easy by some kind of fingerprints
+          delay(n).then(() =>
+            request(app)
+              .post(urls.login)
+              .set('Accept', 'application/json')
+              .send({ password, email }),
+          ),
+        ),
+      );
+
+      logins.forEach(({ status }) => {
+        expect(status).toEqual(200);
+      });
+
+      const refreshes = await Promise.all(
+        logins.map(({ body: { refreshToken } }) =>
+          request(app)
+            .post(urls.refresh)
+            .set('Accept', 'application/json')
+            .send({ refreshToken }),
+        ),
+      );
+
+      refreshes.forEach(({ status, body: { token, refreshToken } }) => {
+        expect(status).toEqual(200);
+        expect(token).toEqual(expect.any(String));
+        expect(refreshToken).toEqual(expect.any(String));
+      });
+    });
+
+    it('should be able to logout from all devices (if change password etc.)', async () => {
+      const tokenPairs: { token: string; refreshToken: string }[] = [];
+      // eslint-disable-next-line no-restricted-syntax
+      for (const value of [0, 1100, 2200]) {
+        // eslint-disable-next-line no-await-in-loop
+        await delay(value);
+        const pair = signTokens({ userId, userRole });
+        // eslint-disable-next-line no-await-in-loop
+        await tokenRepository.save(new RefreshToken(pair.refreshToken, userId));
+        tokenPairs.push(pair);
+      }
+
+      const clientToLogout = 0;
+      const pair = tokenPairs[clientToLogout];
+
+      (await Promise.all(
+        tokenPairs.map(({ token }) =>
+          request(app)
+            .get(urls.test)
+            .set('Accept', 'application/json')
+            .set('Authorization', token),
+        ),
+      )).forEach(({ status }) => expect(status).toEqual(200));
+
+      await request(app)
+        .post(urls.logout)
+        .set('Accept', 'application/json')
+        .send({
+          refreshToken: pair.refreshToken,
+          closeAllSessions: true,
+        });
+
+      (await Promise.all(
+        tokenPairs.map(({ refreshToken }) =>
+          request(app)
+            .post(urls.refresh)
+            .send({ refreshToken }),
+        ),
+      )).forEach(({ status }) => expect(status).toEqual(401));
+    });
+
+    it('should be able to logout independently by each token', async () => {
+      const tokenPairs: { token: string; refreshToken: string }[] = [];
+      // eslint-disable-next-line no-restricted-syntax
+      for (const value of [0, 1100, 2200]) {
+        // eslint-disable-next-line no-await-in-loop
+        await delay(value);
+        const pair = signTokens({ userId, userRole });
+        // eslint-disable-next-line no-await-in-loop
+        await tokenRepository.save(new RefreshToken(pair.refreshToken, userId));
+        tokenPairs.push(pair);
+      }
+
+      const clientToLogout = 0;
+      const pair = tokenPairs[clientToLogout];
+
+      (await Promise.all(
+        tokenPairs.map(({ token }) =>
+          request(app)
+            .get(urls.test)
+            .set('Accept', 'application/json')
+            .set('Authorization', token),
+        ),
+      )).forEach(({ status }) => expect(status).toEqual(200));
+
+      await request(app)
+        .post(urls.logout)
+        .set('Accept', 'application/json')
+        .send({
+          refreshToken: pair.refreshToken,
+          closeAllSessions: false,
+        });
+
+      (await Promise.all(
+        tokenPairs.map(({ refreshToken }) =>
+          request(app)
+            .post(urls.refresh)
+            .send({ refreshToken }),
+        ),
+      )).forEach(({ status }, i) => expect(status).toEqual(i === clientToLogout ? 401 : 200));
     });
   });
 });
