d044102defa93a266878423eed95e7c97a7e1d3b
Mikhail Hozhy <mikhail_hozhy@epam.com>
2019-05-04 13:37:46 +0300

    EPMCUXDA-6609 distinguish db and db for tests

--- config/test.ts
+++ config/test.ts
@@ -1,5 +1,11 @@
 export default {
   dbConfig: {
+    url: process.env.PG_TEST_URL || null,
+    host: process.env.PG_TEST_HOST || 'localhost',
+    port: Number.parseInt(process.env.PG_TEST_PORT as string, 10) || 5432,
+    database: process.env.PG_TEST_DBNAME || 'ptushki-test',
+    username: process.env.PG_TEST_USER || 'username',
+    password: process.env.PG_TEST_PASSWORD || 'password',
     dropSchema: true,
   },
   userCrypto: {
