465c89a7bb9ac6b737628ee63a1bfd6c1f30bc69
Serafimovich Eugene <Eugene.serafimovich@gmail.com>
2019-05-08 01:17:29 +0300

    EPMCUXDA-6640: init pageable getting observations POC

--- src/controllers/observation-controller.ts
+++ src/controllers/observation-controller.ts
@@ -2,9 +2,22 @@ import { NextFunction, Request, Response, Router } from 'express';
 import { getRepository, Repository } from 'typeorm';
 import AbstractController from './abstract-controller';
 import { Observation } from '../entities/observation-entity';
+// import { auth } from '../services/auth-service';
+
+// const DEFAULT_QUERY = '*';
+const DEFAULT_PAGE_NUMBER = '0';
+const DEFAULT_PAGE_SIZE = '5';
 
 interface RequestWithObservation extends Request {
   observation: Observation;
+}
+
+interface RequestWithPageParams extends Request {
+  query: {
+    search: string;
+    pageNumber: string;
+    pageSize: string;
+  };
 }
 
 export default class ObservationController extends AbstractController {
@@ -17,7 +30,9 @@ export default class ObservationController extends AbstractController {
     this.observations = getRepository(Observation);
     this.setMainEntity(this.observations, 'observation');
 
-    this.router.get('/', this.find);
+    this.router.get('/', this.findObservations);
+    this.router.post('/', this.addObservation);
+
     this.router.param('id', this.checkId);
     this.router.get('/:id', this.findOne);
     this.router.delete('/:id', this.remove);
@@ -35,13 +50,47 @@ export default class ObservationController extends AbstractController {
     }
   };
 
-  private find = async (_req: Request, res: Response, next: NextFunction): Promise<void> => {
+  private findObservations = async (req: RequestWithPageParams, res: Response, next: NextFunction): Promise<void> => {
     try {
-      const observations = await this.observations.find();
+      const {
+        // search = DEFAULT_QUERY,
+        pageNumber = DEFAULT_PAGE_NUMBER,
+        pageSize = DEFAULT_PAGE_SIZE,
+      } = req.query;
 
-      res.json(observations);
+      const number = Number.parseInt(pageNumber, 10);
+      const size = Number.parseInt(pageSize, 10);
+
+      const observationsPromise = this.observations.find({
+        skip: number * size,
+        take: size,
+      });
+      const [observations, count] = await Promise.all([observationsPromise, this.observations.count()]);
+      res.json({
+        content: observations,
+        pageNumber: number,
+        pageSize: size,
+        totalElements: count,
+      });
     } catch (e) {
       next(e);
+    }
+  };
+
+  private addObservation = async (req: Request, res: Response, next: NextFunction) => {
+    const newObservation = req.body;
+    if (!req.user) {
+      return res.status(401).send();
+    }
+
+    // validation of Observation fields should be somewhere here
+
+    try {
+      const observation = await Observation.create({ ...newObservation, finder: req.user.id });
+      const result = await this.observations.save(observation);
+      return res.json(result);
+    } catch (e) {
+      return next(e);
     }
   };
 
