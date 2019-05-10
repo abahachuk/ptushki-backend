defaed0a419a90b6b114a5f1186ef0186eb3639a
Mikhail Hozhy <mikhail.hozhy@gmail.com>
2019-05-11 01:26:37 +0300

    EPMCUXDA-6594 extend Species from dictionary

--- src/entities/euring-codes/species-entity.ts
+++ src/entities/euring-codes/species-entity.ts
@@ -1,11 +1,12 @@
 import { Entity, PrimaryColumn, Column, OneToMany } from 'typeorm';
+import { Dictionary } from './common-interfaces';
 import { Ring } from '../ring-entity';
 import { BasaRing } from '../basa-ring-entity';
 import { Observation } from '../observation-entity';
 
 // Related tables in access 'Species' and 'Species by Schem'
 @Entity()
-export class Species {
+export class Species implements Dictionary {
   @PrimaryColumn()
   public id: string;
 
@@ -20,6 +21,15 @@ export class Species {
 
   @Column('varchar', { nullable: true, default: null })
   public family: string | null;
+
+  @Column('varchar', { nullable: true, default: null })
+  public desc_eng: string | null;
+
+  @Column('varchar', { nullable: true, default: null })
+  public desc_rus: string | null;
+
+  @Column('varchar', { nullable: true, default: null })
+  public desc_byn: string | null;
 
   @OneToMany(() => Ring, m => m.speciesMentioned)
   public mentionedInRing: Ring[];
