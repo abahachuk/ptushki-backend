8ed4723b812659e24894a5d852dc1dbe16862b81
Mikhail Hozhy <mikhail.hozhy@gmail.com>
2019-05-11 01:19:25 +0300

    EPMCUXDA-6594 extend scheme of status ring with dictionary

--- src/entities/euring-codes/status-of-ring-entity.ts
+++ src/entities/euring-codes/status-of-ring-entity.ts
@@ -1,14 +1,21 @@
 import { Entity, PrimaryColumn, Column, OneToMany } from 'typeorm';
+import { Dictionary } from './common-interfaces';
 import { Ring } from '../ring-entity';
 
 // Related table in access 'Status of ring'
 @Entity()
-export class StatusOfRing {
+export class StatusOfRing implements Dictionary {
   @PrimaryColumn()
   public id: string;
 
   @Column('varchar', { nullable: true, default: null })
-  public description: string | null;
+  public desc_eng: string | null;
+
+  @Column('varchar', { nullable: true, default: null })
+  public desc_rus: string | null;
+
+  @Column('varchar', { nullable: true, default: null })
+  public desc_byn: string | null;
 
   @OneToMany(() => Ring, m => m.statusOfRing)
   public ring: Ring[];
