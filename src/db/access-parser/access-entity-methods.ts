563c509bd6399fab1f825fff471565f45d7daad9
Mikhail Hozhy <mikhail.hozhy@gmail.com>
2019-05-13 02:05:44 +0300

    EPMCUXDA-6815 add async generator for getting records

--- src/db/access-parser/access-entity-methods.ts
+++ src/db/access-parser/access-entity-methods.ts
@@ -1,5 +1,25 @@
 import accessConnection from './access-connection';
 
+export async function* getEntityRecords(
+  table: string,
+  id: string,
+  amount: number = 1000,
+): AsyncIterableIterator<any[]> {
+  let lastId;
+  let records: any[];
+  while (true) {
+    // eslint-disable-next-line no-await-in-loop
+    records = await accessConnection.query(
+      `SELECT TOP ${amount} * FROM ${table}${lastId ? ` WHERE ${id} > "${lastId}"` : ''};`,
+    );
+    if (!records.length) {
+      break;
+    }
+    lastId = records[records.length - 1][id];
+    yield records;
+  }
+}
+
 export async function entityCount(table: string, id: string): Promise<number> {
   const count: number = await accessConnection.query(`SELECT COUNT(${id}) FROM ${table};`);
   return count;
