d6db3b09166d13a8d9fedf3f4468c5908ee62c4e
Mikhail Hozhy <mikhail.hozhy@gmail.com>
2019-05-12 16:20:07 +0300

    EPMCUXDA-6815 add simple limited rings loader

--- src/db/access-parser/run.ts
+++ src/db/access-parser/run.ts
@@ -4,12 +4,14 @@ import ADODB from 'node-adodb';
 import { Connection, createConnection, getRepository, Repository } from 'typeorm';
 import config from '../prepare-db-config';
 import { EURINGs } from './EURING-access-tables';
+import { ringMapper, RingMapper } from './rings-access-table';
+import { Ring } from '../../entities/ring-entity';
 
 const accessConnection = ADODB.open(
   'Provider=Microsoft.ACE.OLEDB.12.0;Data Source=MBCRB(2007-30.01.2012).mdb;Persist Security Info=False;',
 );
 
-async function query(table: string): Promise<any[]> {
+async function getEuring(table: string): Promise<any[]> {
   try {
     const { Entity, mapping } = EURINGs[table];
     const keys: string[] = [...mapping.keys()];
@@ -32,9 +34,39 @@ async function query(table: string): Promise<any[]> {
   }
 }
 
-async function load(instances: any[], tableName: string) {
+async function loadEURING(instances: any[], tableName: string): Promise<void> {
   const { Entity } = EURINGs[tableName];
   const repository: Repository<any> = getRepository(Entity);
+  await repository.insert(instances);
+  console.log(tableName, ' inserted');
+}
+
+async function getRings(): Promise<Ring[]> {
+  const limit = 100;
+  const dbRings: any[] = await accessConnection.query(`SELECT TOP ${limit} * FROM Ringby`);
+  const rings: Ring[] = dbRings
+    .map((dbRing: any) => {
+      try {
+        const ring = Object.keys(ringMapper).reduce(
+          (acc: { [index in keyof RingMapper]: any }, key: keyof RingMapper) => {
+            const map = ringMapper[key];
+            acc[key] = typeof map === 'function' ? map(dbRing) : dbRing[map];
+            return acc;
+          },
+          {},
+        );
+        return ring;
+      } catch (e) {
+        return null;
+      }
+    })
+    .filter(ring => !!ring)
+    .map(mapped => Object.assign(new Ring(), mapped));
+  return rings;
+}
+
+async function loadRings(instances: any[]): Promise<void> {
+  const repository: Repository<any> = getRepository(Ring);
   await repository.insert(instances);
 }
 
@@ -47,12 +79,15 @@ let db: Connection | undefined;
     // eslint-disable-next-line no-restricted-syntax
     for (const tableName of Object.keys(EURINGs)) {
       // eslint-disable-next-line no-await-in-loop
-      const instances = await query(tableName);
+      const instances = await getEuring(tableName);
       if (instances.length) {
         // eslint-disable-next-line no-await-in-loop
-        await load(instances, tableName);
+        await loadEURING(instances, tableName);
       }
     }
+
+    const rings = await getRings();
+    await loadRings(rings);
   } catch (e) {
     console.log(e);
   }
