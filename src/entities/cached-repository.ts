fac2cd8e76017dd93ca3217ecff71228fc33dad9
Mikhail Hozhy <mikhail_hozhy@epam.com>
2019-05-23 06:30:02 +0300

    EPMCUXDA-6851 update cached repository by extending Repository

--- src/entities/cached-repository.ts
+++ src/entities/cached-repository.ts
@@ -1,21 +1,18 @@
-import { EntityManager, ObjectType } from 'typeorm';
+import { Repository } from 'typeorm';
 import cache from './repositories-cache';
 
-export class CachedRepository<T> {
-  public entity: ObjectType<T>;
+const camelCase = (s: string) => s[0].toLowerCase() + s.slice(1);
 
-  private manager: EntityManager;
-
-  protected constructor(manager: EntityManager, entity: ObjectType<T>) {
-    this.manager = manager;
-    this.entity = entity;
-  }
-
+export class CachedRepository<T> extends Repository<T> {
   public async find(): Promise<T[]> {
-    const { name: key } = this.entity;
+    const { name: key } = this.metadata;
     if (!cache.has(key)) {
-      cache.set(key, await this.manager.find(this.entity));
+      cache.set(key, await super.find());
     }
     return cache.get(key);
+  }
+
+  public get tableName(): string {
+    return camelCase(this.metadata.name);
   }
 }
