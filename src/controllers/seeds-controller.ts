c697e6057905bb06cf10527af4806ccef86d4958
Mikhail Hozhy <mikhail_hozhy@epam.com>
2019-05-23 06:40:42 +0300

    EPMCUXDA-6851 update seeds endpoint

--- src/controllers/seeds-controller.ts
+++ src/controllers/seeds-controller.ts
@@ -1,12 +1,20 @@
 import { NextFunction, Request, Response, Router } from 'express';
-// import { getRepository, Repository } from 'typeorm';
+import { getCustomRepository, Repository } from 'typeorm';
+
 import AbstractController from './abstract-controller';
+import { cachedEURINGCodes } from '../entities/euring-codes/cached-entities-fabric';
+import { CachedRepository } from '../entities/cached-repository';
 
 export default class SeedsController extends AbstractController {
   private router: Router;
 
+  public cached: Repository<any>[];
+
   public init(): Router {
     this.router = Router();
+    this.cached = Object.keys(cachedEURINGCodes).map((entityName: string) =>
+      getCustomRepository(cachedEURINGCodes[entityName]),
+    );
 
     this.router.get('/', this.getSeeds);
 
@@ -15,8 +23,15 @@ export default class SeedsController extends AbstractController {
 
   private getSeeds = async (_req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      res.json({});
+      // const { ln: language } = req.query;
+      const result = await Promise.all(
+        this.cached.map(async (repository: CachedRepository<any>) => ({
+          [repository.tableName]: await repository.find(),
+        })),
+      );
+      res.json(result);
     } catch (e) {
+      console.log(e);
       next(e);
     }
   };
