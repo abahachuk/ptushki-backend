000457fd8297429c1739edc8c8c07fe4bd7d3ce9
Mikhail Hozhy <mikhail_hozhy@epam.com>
2019-06-10 14:06:41 +0300

    EPMCUXDA-6946 fix typo

--- src/fixtures/AccuracyOfCoordinatesProcessor.ts
+++ src/fixtures/AccuracyOfCoordinatesProcessor.ts
@@ -2,7 +2,7 @@ import { IProcessor } from 'typeorm-fixtures-cli';
 import { AccuracyOfCoordinates } from '../entities/euring-codes';
 
 /* eslint-disable class-methods-use-this */
-export default class AccuracyofCoordinatesProcessor implements IProcessor<AccuracyOfCoordinates> {
+export default class AccuracyOfCoordinatesProcessor implements IProcessor<AccuracyOfCoordinates> {
   public preProcess(name: string, object: any): any {
     return {
       ...object,
