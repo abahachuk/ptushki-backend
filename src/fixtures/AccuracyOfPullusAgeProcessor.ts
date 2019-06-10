afa845ec23ec9d9bd2e20fd3885f18975dcfe633
Mikhail Hozhy <mikhail_hozhy@epam.com>
2019-06-10 14:12:34 +0300

    EPMCUXDA-6946 fix fixture for accuracy of pullus age

--- src/fixtures/AccuracyOfPullusAgeProcessor.ts
+++ src/fixtures/AccuracyOfPullusAgeProcessor.ts
@@ -1,12 +1,15 @@
 import { IProcessor } from 'typeorm-fixtures-cli';
 import { AccuracyOfPullusAge } from '../entities/euring-codes';
 
-/* eslint-disable class-methods-use-this */
+/* eslint-disable */
+const makeid = (i: number): string => '-0123456789U'[i];
+
+/* class-methods-use-this */
 export default class AccuracyOfPullusAgeProcessor implements IProcessor<AccuracyOfPullusAge> {
   public preProcess(name: string, object: any): any {
     return {
       ...object,
-      id: parseInt(name.replace(/\D/g, ''), 10),
+      id: makeid(parseInt(name.replace(/\D/g,''))),
     };
   }
 }
