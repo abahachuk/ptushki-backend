6820c219b9c9491e3931a2f3cb72742a205215b6
slabkodima90 <slabkodima90@gmail.com>
2019-06-11 13:18:09 +0300

    - Added template for import

--- src/routes.ts
+++ src/routes.ts
@@ -5,7 +5,6 @@ import {
   ObservationController,
   RingsByController,
   InitialDataController,
-  ImportController,
 } from './controllers';
 import { auth } from './services/auth-service';
 
@@ -14,9 +13,8 @@ const routes: Router = Router();
 export default (): Router => {
   routes.use('/auth', new AuthController().init());
   routes.use('/users', new UsersController().init());
-  routes.use('/observations', auth.required, new ObservationController().init());
+  routes.use('/observations', new ObservationController().init());
   routes.use('/rings-by', auth.required, new RingsByController().init());
   routes.use('/initial-data', auth.required, new InitialDataController().init());
-  routes.use('/import', auth.required, new ImportController().init());
   return routes;
 };
