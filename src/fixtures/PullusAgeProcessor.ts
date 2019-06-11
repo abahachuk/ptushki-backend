5622abda34eb54fcdad3967d3f289613fc9f8462
Mikhail Hozhy <mikhail_hozhy@epam.com>
2019-06-11 04:05:18 +0300

    EPMCUXDA-6946 fix processor's name & amount

--- src/fixtures/PullusAgeProcessor.ts
+++ src/fixtures/PullusAgeProcessor.ts
@@ -2,7 +2,7 @@ import { IProcessor } from 'typeorm-fixtures-cli';
 import { PullusAge } from '../entities/euring-codes';
 
 const ids = ['--'].concat(
-  Array(99)
+  Array(100)
     .fill('')
     .map((_n, i) => i.toString().padStart(2, '0')),
 );
@@ -12,6 +12,7 @@ const makeid = (i: number): string => ids[i];
 /* eslint-disable class-methods-use-this */
 export default class PullusAgeProcessor implements IProcessor<PullusAge> {
   public preProcess(name: string, object: any): any {
+    console.log(makeid(parseInt(name.replace(/\D/g, ''), 10)));
     return {
       ...object,
       id: makeid(parseInt(name.replace(/\D/g, ''), 10)),
