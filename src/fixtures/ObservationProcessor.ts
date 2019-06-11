d2334fe3b103d17d05b0d77108e6aaecc393a353
Mikhail Hozhy <mikhail_hozhy@epam.com>
2019-06-11 03:35:03 +0300

    EPMCUXDA-6946 update fixtures Observation

--- src/fixtures/ObservationProcessor.ts
+++ src/fixtures/ObservationProcessor.ts
@@ -4,7 +4,10 @@ import { Observation } from '../entities/observation-entity';
 export default class ObservationProcessor implements IProcessor<Observation> {
   /* eslint-disable-next-line class-methods-use-this */
   public postProcess(_name: string, object: Observation): void {
-    /* eslint-disable-next-line no-param-reassign */
+    /* eslint-disable no-param-reassign */
     object.ringMentioned = object.ring.identificationNumber;
+    object.distance = object.distance ? object.distance.toString().padStart(5, '0') : '-----';
+    object.direction = object.direction ? object.direction.toString().padStart(3, '0') : '---';
+    object.elapsedTime = object.elapsedTime ? object.elapsedTime.toString().padStart(5, '0') : '-----';
   }
 }
