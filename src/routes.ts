3d386efaad9edee270dadcc933c992122e6a910a
Mikhail Hozhy <mikhail_hozhy@epam.com>
2019-06-13 00:13:32 +0300

    EPMCUXDA-6851 made routes initializations async

--- src/routes.ts
+++ src/routes.ts
@@ -10,11 +10,13 @@ import { auth } from './services/auth-service';
 
 const routes: Router = Router();
 
-export default (): Router => {
+export default async (): Promise<Router> => {
+  const initialDataController = new InitialDataController();
   routes.use('/auth', new AuthController().init());
   routes.use('/users', new UsersController().init());
   routes.use('/observations', auth.required, new ObservationController().init());
   routes.use('/rings-by', auth.required, new RingsByController().init());
-  routes.use('/initial-data', auth.required, new InitialDataController().init());
+  routes.use('/initial-data', auth.required, initialDataController.init());
+  await initialDataController.heatUp();
   return routes;
 };
