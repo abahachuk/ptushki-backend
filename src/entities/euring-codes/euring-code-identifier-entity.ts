e1d0a5c4572023fa431e5f9a26c5dcdef2f90f4f
Mikhail Hozhy <mikhail_hozhy@epam.com>
2019-11-25 01:55:58 +0300

    add missed EURING fields to observation

--- src/entities/euring-codes/euring-code-identifier-entity.ts
+++ src/entities/euring-codes/euring-code-identifier-entity.ts
@@ -2,6 +2,7 @@ import { Entity, PrimaryColumn, Column, OneToMany } from 'typeorm';
 import { IsInt, Min, Max, IsOptional, IsString } from 'class-validator';
 import { EntityDto } from '../common-interfaces';
 import { Ring } from '../ring-entity';
+import { Observation } from '../observation-entity';
 
 // Related table in access 'Euring cod identifier'
 @Entity()
@@ -29,4 +30,7 @@ export class EURINGCodeIdentifier implements EntityDto {
 
   @OneToMany(() => Ring, m => m.euringCodeIdentifier)
   public ring: Ring[];
+
+  @OneToMany(() => Observation, m => m.euringCodeIdentifier)
+  public observation: Observation[];
 }
