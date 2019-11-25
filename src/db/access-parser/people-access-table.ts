3638d14e6e4fc6f19ff4063081df2ab3c8dd83f2
Mikhail Hozhy <mikhail_hozhy@epam.com>
2019-11-25 03:22:45 +0300

    add handler to get rid of persons duplicates and missed ownerships

--- src/db/access-parser/people-access-table.ts
+++ src/db/access-parser/people-access-table.ts
@@ -2,10 +2,27 @@ import { Person } from '../../entities/person-entity';
 import { logger } from '../../utils/logger';
 
 type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
-export type PersonMap = { [index in keyof Omit<Person, 'id' | 'ring' | 'observation'>]: string };
+export type PersonMap = {
+  [index in keyof Omit<Person, 'id' | 'ring' | 'observation'>]: ((...args: any) => any) | string
+};
+
+export const trimName = (name: string = ''): string => name.trim().replace(/\s\s+/g, ' ');
+
+const name = (item: any, set: Set<string>): string | null => {
+  const processedName = trimName(item.Ringer);
+  if (!processedName) {
+    return null;
+  }
+  if (set.has(processedName.toLowerCase())) {
+    throw new Error(`Duplicated person: ${processedName}`);
+  }
+
+  set.add(processedName.toLowerCase());
+  return processedName;
+};
 
 export const personMap: PersonMap = {
-  name: 'Ringer',
+  name,
   email: 'e-mail',
   code: 'Ringer cod',
   phone: 'Tel mob',
@@ -16,12 +33,13 @@ export const personMap: PersonMap = {
 const personsKeys = Object.keys(personMap);
 
 export function peopleMapper(dbRecords: any[]): Person[] {
+  const personSet = new Set();
   const persons: Person[] = dbRecords
     .map((dbPerson: any) => {
       try {
         const observation = personsKeys.reduce((acc: { [index in keyof PersonMap]: any }, key: keyof PersonMap) => {
           const map = personMap[key];
-          acc[key] = dbPerson[map];
+          acc[key] = typeof map === 'function' ? map(dbPerson, personSet) : dbPerson[map];
           return acc;
         }, {});
         return observation;
