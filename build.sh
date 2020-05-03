d6b71b55515b317a4278f660be114c3f77ac9631
Yauheni Serafimovich <yauheni_serafimovich@epam.com>
2020-05-03 18:31:22 +0300

    clear redundant files

--- build.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-npm install
-docker build -t ptushki-api .
-docker tag ptushki-api:latest ptushkiregistry.azurecr.io/ptushki-api:latest
-docker login ptushkiregistry.azurecr.io
-docker push ptushkiregistry.azurecr.io/ptushki-api:latest
