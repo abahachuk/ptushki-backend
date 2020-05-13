a56ad8ebf016218cabf8135e42b470a2c9b8bf2d
Mikhail Hozhy <mikhail_hozhy@epam.com>
2020-05-13 08:48:37 +0300

    add tuple type for euring codes from parsed string

--- src/entities/common-interfaces.ts
+++ src/entities/common-interfaces.ts
@@ -1,3 +1,67 @@
+export type EURINGParsedString = [
+  string, // ringingScheme
+  string, // primaryIdentificationMethod
+  string, // identificationNumber
+  string, // verificationOfTheMetalRing
+  string, // metalRingInformation
+  string, // otherMarksInformation
+  string, // speciesMentioned
+  string, // speciesConcluded
+  string, // manipulated
+  string, // movedBeforeTheCapture
+  string, // catchingMethod
+  string, // catchingLures
+  string, // sexMentioned
+  string, // sexConcluded
+  string, // ageMentioned
+  string, // ageConcluded
+  string, // status
+  string, // broodSize
+  string, // pullusAge
+  string, // accuracyOfPullusAge
+  string, // date
+  string, // accuracyOfDate
+  string, // time
+  string, // placeCode
+  string, // latitudeLongitude
+  string, // accuracyOfCoordinates
+  string, // condition
+  string, // circumstances
+  string, // circumstancesPresumed
+  string, // euringCodeIdentifier
+  string, // distance
+  string, // direction
+  string, // elapsedTime
+  // Below params except "placeName" "remarks" "reference" are unsupported by US
+  string, // wingLength
+  string, // thirdPrimary
+  string, // stateOfWingPoint
+  string, // mass
+  string, // moult
+  string, // plumageCode
+  string, // hindClaw
+  string, // billLength
+  string, // billMethod
+  string, // totalHeadLength
+  string, // tarsus
+  string, // tarsusMethod
+  string, // tailLength
+  string, // tailDiffernce
+  string, // fatScore
+  string, // fatScoreMethod
+  string, // pectoralMuscle
+  string, // broodPatch
+  string, // primaryScore
+  string, // primaryMoult
+  string, // oldGreaterCoverts
+  string, // alula
+  string, // carpalCovert
+  string, // sexingMethod
+  string, // placeName
+  string, // remarks
+  string // reference
+];
+
 export interface EURINGCodes {
   identificationNumber: any;
   ringingScheme: any;
